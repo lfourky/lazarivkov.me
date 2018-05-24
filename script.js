10a7503ba52cc88d0cb8a253ae4c1f6b683590de
Lazar Ivkov <lfourky@gmail.com>
2018-05-24 14:44:28 +0200

    progress update

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var progress = [
 	{
 		"name": "Completing a course: <a href='https://www.pluralsight.com/courses/getting-started-kubernetes' target='_blank'>Getting Started with Kubernetes</a>",
         "currentValue": 0,
-        "currentText": "",
+        "currentText": "Completed",
 		"lastWeeksProgress": 100,
         "weeklyProgress": 0
 	},
 	{
 		"name": "Completing a course: <a href='https://www.pluralsight.com/courses/implementing-devops-real-world' target='_blank'>Implementing DevOps in the Real World</a>",
         "currentValue": 0,
-        "currentText": "",
+        "currentText": "Completed",
 		"lastWeeksProgress": 100,
         "weeklyProgress": 0
 	},
