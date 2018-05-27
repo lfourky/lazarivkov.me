f3452e2fb51ee7a35c549c11d412a41eab1a82fa
Lazar Ivkov <lfourky@gmail.com>
2018-05-27 21:14:38 +0200

    progress update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var progress = [
         "currentValue": 0,
         "currentText": "",
 		"lastWeeksProgress": 0,
-        "weeklyProgress": (228/354)*100
+        "weeklyProgress": (237/354)*100
     },
 	{
 		"name": "Completing a course: <a href='https://www.pluralsight.com/courses/getting-started-kubernetes' target='_blank'>Getting Started with Kubernetes</a>",
