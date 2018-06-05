510c20cc79158a6fbbf92f2aeba9ae10a4eaf683
Lazar Ivkov <lfourky@gmail.com>
2018-06-05 20:00:42 +0200

    progress update

--- script.js
+++ script.js
@@ -55,13 +55,6 @@ var progress = [
 		"lastWeeksProgress": 0,
         "weeklyProgress": 0
     },
-    {
-        "name": "Solving programming puzzles with <a href='https://golang.org/' target='_blank'>Go</a> and C++ on: <a href='https://www.codingame.com/home' target='_blank'>CodinGame</a>",
-        "currentValue": (33/48) * 100,
-        "currentText": "",
-		"lastWeeksProgress": 0,
-        "weeklyProgress": 0
-    },
 	{
         "name": "Completing a language course: <a href='http://www.pimsleur.com/learn-french' target='_blank'>Pimsleur French (part 2)</a>",
         "currentValue": 51,
