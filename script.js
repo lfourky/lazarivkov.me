55808270797f1f5f336cc3a60311c29ecfc06bb6
Lazar Ivkov <lfourky@gmail.com>
2018-12-10 22:46:31 +0100

    progress update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var progress = [
         "currentValue": ((243)/613)*100,
         "currentText": "",
 		"lastWeeksProgress": ((264-243)/613)*100,
-        "weeklyProgress": 0
+        "weeklyProgress": ((272-264)/613)*100
     },
 	{
         "name": "Reading a book: <a href='http://shop.oreilly.com/product/0636920132448.do' target='_blank'>The Site Reliability Workbook</a>",
