28fbbd7c13330cfab0f7bb8526b3d929656adc6d
Lazar Ivkov <lfourky@gmail.com>
2019-01-01 21:01:08 +0100

    progress update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var progress = [
       "name": "Listening to an audiobook <a href='https://www.audible.com/pd/Chaos-Audiobook/B004I7C00O?ref=a_lib_c4_libItem_0_B004I7C00O&pf_rd_p=ae76b2bb-e63d-4a67-b357-dab3dee05ca1&pf_rd_r=Y5FB770H8Q672GTQRA7H&' target='_blank'>Chaos - Making a New Science</a>",
       "currentValue": 0,
       "currentText": "",
-      "lastWeeksProgress": (653-602)/653*100,
+      "lastWeeksProgress": (653-547)/653*100,
       "weeklyProgress": 0
    },
    {
