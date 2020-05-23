17ca52d7afffeb13a668c3397cef9371e3276bdc
Lazar Ivkov <lfourky@gmail.com>
2020-05-23 22:18:44 +0200

    progress update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const BOOK = "book";
 var progress = [
    progress(BOOK, "http://shop.oreilly.com/product/0636920021575.do", "REST API Design Rulebook", 0,100,0),
    progress(AUDIOBOOK, "https://www.audible.com/pd/From-Bacteria-to-Bach-and-Back-Audiobook/B01MYBBU13", "From Bacteria to Bach and Back", 5, 0, 0),
-   progress(BOOK, "https://www.manning.com/books/event-processing-in-action", "Event Processing in Action", 0,0,20),
+   progress(BOOK, "https://www.manning.com/books/event-processing-in-action", "Event Processing in Action", 0,20,0),
    progress(BOOK, "https://www.distributed-systems.net/index.php/books/distributed-systems-3rd-edition-2017/", "Distributed Systems", 42,0,0),
    progress(COURSE, "https://www.educative.io/collection/5668639101419520/5649050225344512", "Grokking the System Design Interview", (3/28)*100,0,0)
 ]
